fc168a57ae4ebb7e7aedbfea734f885606d5979d
warrenwtom <45927900+warrenwtom@users.noreply.github.com>
2019-01-11 14:51:51 -0500

    updated test code for different test

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -4,7 +4,16 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*int x = 7;
+		if(x < 42) {
+		   System.out.println("Hi");
+		}
 		System.out.println("Hello world!");
+		*/
+		int [ ] myArr = {1,1,2,3,5,8,13,21,34,55};
+		for(int t: myArr) {
+		   System.out.println(t);
+		}
 	}
 
 }
