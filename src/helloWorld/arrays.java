e3081552990bfddebc0110e5a956d84bc94e1c3c
warrenwtom <45927900+warrenwtom@users.noreply.github.com>
2019-01-23 15:11:12 -0500

    separating out the code to appropriate classes

---
+++ src/helloWorld/arrays.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class arrays {
+	public static void main(String[] args) {
+		int [ ] myArr = {1,1,2,3,5,8,13,21,34,55};
+		for(int t: myArr) {
+		   System.out.println(t);
+		}
+
+	}
+}
---
+++ src/helloWorld/variables.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class variables {
+	public static void main(String[] args) {
+		int x = 7;
+		if(x < 42) {
+		   System.out.println("Hi");
+		}
+	}
+}
